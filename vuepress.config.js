a212727edbe9720561562185db9ad209e9d6a710
Hemanth Narravula <hemanth.narravula@ttc.com>
2022-07-27 23:19:38 +0100

    change base url

--- vuepress.config.js
+++ vuepress.config.js
@@ -4,6 +4,7 @@ const { gitPlugin } = require('@vuepress/plugin-git')
 module.exports = {
     lang: 'en-US',
     title: 'Payment Applications Docs',
+    base: '/payment-applications-docs/',
     description: 'Payment Application Docs Helps us to understand the payment prodcuts and services we have',
     // https://ttc.com/wp-content/uploads/2019/12/cropped-cropped-cropped-ttc-favicon-32x32-1-32x32.jpg
     head: [['link', { rel: 'icon', href: 'https://ttc.com/wp-content/uploads/2019/12/cropped-cropped-cropped-ttc-favicon-32x32-1-32x32.jpg' }]],    
